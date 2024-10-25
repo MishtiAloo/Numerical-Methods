a85fa328b8f20b685a08bc630c0e66113b5314bf
srsn63 <rahul115415@gmai.com>
2024-10-25 19:40:28 +0600

    Gauss-Seidel added

--- Numerical_Methods.cpp
+++ Numerical_Methods.cpp
@@ -1,9 +1,60 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
- 
+ void Gauss_Seidel()
+ {
+        cout << "Enter the number of variables : ";
+    int n;
+    cin >> n;
+
+  
+
+    vector<vector<float>> coefficients(n, vector<float>(n));
+    vector<float> constants(n);
+    vector<float> current(n, 0), previous(n, 0);
+    float tolerance;
+
+    cout << "Enter the tolerance level: ";
+    cin >> tolerance;
+
+    // Input coefficients and constants for each equation
+    for (int i = 0; i < n; i++) {
+        cout << "Enter coefficients and constant term for equation " << i + 1 << ": ";
+        for (int j = 0; j < n; j++) {
+            cin >> coefficients[i][j];
+        }
+        cin >> constants[i];
+    }
+
+    int iteration = 1;
+    vector<float> errors(n, 0.0);
+
+    // (Gauss-Seidel)
+    do {
+        cout << "Iteration: " << iteration << endl;
+        for (int i = 0; i < n; i++) {
+            float sum = constants[i];
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    sum -= coefficients[i][j] * current[j];
+                }
+            }
+            previous[i] = current[i];
+            current[i] = sum / coefficients[i][i];
+            errors[i] = fabs(current[i] - previous[i]);
+            cout << "Variable " << i + 1 << " = " << current[i] << endl;
+        }
+        iteration++;
+    } while (*max_element(errors.begin(), errors.end()) > tolerance);
+
+    cout << "\nRoots:- \n";
+    for (int i = 0; i < n; i++) {
+        cout << "Variable " << i + 1 << " = " << current[i] << endl;
+    }
+
+ }
 int main () {
-    cout<<"Tahmid-Rahul-Rezuan";
+    cout<<"HELLO world ";
     int choice;
     while (1) {
         cout << "What do you want to solve?\n\n1.Linear system of equations\n2.Non-Linear equation\n3.Differential equation\n4.Matrix inversion\n5. Exit\n\nYour choice: ";
@@ -36,6 +87,7 @@ int main () {
         }
         case 2: {
             // GS
+            Gauss_Seidel();
             break;
         }
         case 3: {
